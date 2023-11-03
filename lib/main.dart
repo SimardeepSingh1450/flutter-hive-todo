f84acd79beac6d6891f20faf6ce426b0e5666c0e
SimardeepSingh1450 <smudhar_be21@thapar.edu>
2023-11-03 23:28:40 +0530

    add: added hive nosql db

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hivetodo/pages/home_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  //intialize the hive
+  await Hive.initFlutter();
+
+  //open a box
+  var box = await Hive.openBox('myBox');
+
   runApp(const MyApp());
 }
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hivetodo/data/database.dart';
 import 'package:hivetodo/util/dialog_box.dart';
 import 'package:hivetodo/util/todo_tile.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,31 +12,58 @@ class HomePage extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<HomePage> {
+  //reference the hive box
+  final _myBox = Hive.box('myBox');
+  ToDoDataBase db = ToDoDataBase();
+
+  //nit state synatx using flutter-intellisense:
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    //if this is the first time ever opening the app, then create default data
+    if (_myBox.get("TODOLIST") == null) {
+      db.createInitialData();
+    } else {
+      //there already exists data
+      db.loadData();
+    }
+
+    super.initState();
+  }
+
   //text input controller -> for accessing the text value in TextField widget
   final _controller = TextEditingController();
+
   //list of todo tasks
-  List toDoList = [
-    ["Make Tutorial", false],
-    ["Do exercise", false],
-  ];
+  // List toDoList = [
+  //   ["Make Tutorial", false],
+  //   ["Do exercise", false],
+  // ];
 
   //check-box was tapped
   void checkBoxChanged(bool? value, int index) {
     setState(() {
-      toDoList[index][1] = !toDoList[index][1];
+      db.toDoList[index][1] = !db.toDoList[index][1];
     });
+
+    //make db update changes inside hive using updateDatabase
+    db.updateDataBase();
   }
 
   //save new task
   void saveNewTask() {
     setState(() {
-      toDoList.add([_controller.text, false]);
+      db.toDoList.add([_controller.text, false]);
       //clear up the textField content after saving
       _controller.clear();
     });
 
     //We use Navigator.of(context).pop() to terminate the dialog box
     Navigator.of(context).pop();
+
+    //make db update changes inside hive using updateDatabase
+    db.updateDataBase();
   }
 
   //create a new task
@@ -53,8 +82,11 @@ class _MyWidgetState extends State<HomePage> {
   //delete a task
   void deleteTask(int index) {
     setState(() {
-      toDoList.removeAt(index);
+      db.toDoList.removeAt(index);
     });
+
+    //make db update changes inside hive using updateDatabase
+    db.updateDataBase();
   }
 
   @override
@@ -69,11 +101,11 @@ class _MyWidgetState extends State<HomePage> {
         floatingActionButton: FloatingActionButton(
             onPressed: createNewTask, child: Icon(Icons.add)),
         body: ListView.builder(
-            itemCount: toDoList.length,
+            itemCount: db.toDoList.length,
             itemBuilder: (context, index) {
               return ToDoTile(
-                taskCompleted: toDoList[index][1],
-                taskName: toDoList[index][0],
+                taskCompleted: db.toDoList[index][1],
+                taskName: db.toDoList[index][0],
                 onChanged: (value) => checkBoxChanged(value, index),
                 deleteFunction: (context) => deleteTask(index),
               );
